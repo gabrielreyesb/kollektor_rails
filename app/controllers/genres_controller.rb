e315d8dfde98b653c461b2a545baed0ebdbf1063
Gabriel <gabriel@handy.la>
2024-06-27 16:23:42 -0600

    Botón para CRUD con modal

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -1,52 +1,59 @@
 class GenresController < ApplicationController
-  before_action :set_genre
+
+  before_action :set_genre, only: %i[ show edit update destroy ]
 
   def index
     @genres = Genre.all
   end
   
   def show
+    @genre = Genre.find(params[:id]) 
   end
 
   def new
     @genre = Genre.new
-  end
+  end  
 
   def edit
+    @genre = Genre.find(params[:id])
+    respond_to do |format|
+      format.html { render partial: "genres/form", locals: { genre: @genre } }
+      format.turbo_stream { render turbo_stream: turbo_stream.update("genre_modal", partial: "genres/form", locals: { genre: @genre }) }
+    end
   end
 
   def create
     @genre = Genre.new(genre_params)
-
     respond_to do |format|
       if @genre.save
-        format.html { redirect_to genre_url(@genre), notice: "Genre was successfully created." }
-        format.json { render :show, status: :created, location: @genre }
+        format.html { redirect_to genres_path, notice: "Genre was successfully created." }
+        format.turbo_stream
       else
         format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @genre.errors, status: :unprocessable_entity }
+        format.turbo_stream { render turbo_stream: turbo_stream.replace("genre_modal", partial: "genres/form", locals: { genre: @genre }) }
       end
     end
   end
 
   def update
+    @genre = Genre.find(params[:id])
     respond_to do |format|
       if @genre.update(genre_params)
-        format.html { redirect_to genre_url(@genre), notice: "Genre was successfully updated." }
-        format.json { render :show, status: :ok, location: @genre }
+        format.html { redirect_to genres_path, notice: "Genre was successfully updated." }
+        format.turbo_stream 
       else
         format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @genre.errors, status: :unprocessable_entity }
+        format.turbo_stream { render turbo_stream: turbo_stream.replace("genre_modal", partial: "genres/form", locals: { genre: @genre }) }
       end
     end
   end
 
   def destroy
-    @genre.destroy!
-
+    @genre = Genre.find(params[:id])
+    @genre.destroy
     respond_to do |format|
-      format.html { redirect_to genres_url, notice: "Genre was successfully destroyed." }
-      format.json { head :no_content }
+      format.html { redirect_to genres_path, notice: "Genre was successfully destroyed." }
+      format.turbo_stream
     end
   end
 
